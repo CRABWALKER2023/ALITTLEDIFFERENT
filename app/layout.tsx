f1adc911c3b92e99d7e5e852a5f576057610e133
CRABWALKER2023 <efried97@gmail.com>
2026-05-28 18:05:04 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,10 @@
-export const metadata = {
-  title: "Luxe Outlet",
-};
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html>
       <body>{children}</body>
     </html>
   );
